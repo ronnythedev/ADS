8d172ef5867f40ff3a9bfa21dc2b4cbcffe56015
Ronny Delgado <delgado.ronny@gmail.com>
2023-11-03 16:58:44 -0600

    feat: finish algo to compute if second array contains squared values of first array
    O(n) solution

--- ADS/00_FrequencyCounterPattern/001.ArrayContainingSquaredValuesProblem.cs
+++ ADS/00_FrequencyCounterPattern/001.ArrayContainingSquaredValuesProblem.cs
@@ -1,12 +1,60 @@
-﻿using System;
+﻿using System.Collections.Generic;
 
 namespace ADS.FrequencyCounterPattern;
 
 public static class ArrayContainingSquaredValuesProblem
 {
-    public static bool SecondArrayHaveAllSquaredValues(int[] array1, int[] array2)
+    // STATEMENT OF THE PROBLEM
+    // ------------------------
+    // Develop a function that takes two input arrays. 
+    // The function should determine whether each element in the first array has its counterpart, squared, in the second array. 
+    // Additionally, the frequency of occurrence for each value in the first array must match the frequency of the squared value in the second array. 
+    // The function must return a boolean value TRUE if the aforementioned conditions are met, or FALSE otherwise.
+
+    public static bool SameSquared(int[] array1, int[] array2)
     {
-        return false;
+        // In a "brute-force" solution, we would have to use nested loops to compare each element in the first array with each element in the second array.
+        // The time complexity of this solution would be O(n^2), which is not ideal.
+        // A better solution would be to use a frequency counter pattern.
+
+        // If the length of the two arrays is different, we can return false immediately.
+        if (array1.Length != array2.Length)
+        {
+            return false;
+        }
+
+        // We create two dictionaries to store the frequency of each value in the two arrays.
+        var frequencyCounter1 = new Dictionary<int, int>();
+        var frequencyCounter2 = new Dictionary<int, int>();
+
+        // We iterate through each array and store the frequency of each value in the corresponding dictionary.
+        foreach (var value in array1)
+        {
+            frequencyCounter1[value] = frequencyCounter1.GetValueOrDefault(value, 0) + 1;
+        }
+
+        foreach (var value in array2)
+        {
+            frequencyCounter2[value] = frequencyCounter2.GetValueOrDefault(value, 0) + 1;
+        }
+
+        // We iterate through the first dictionary and check whether the squared value of each key is present in the second dictionary. 
+        // If it is not, we return false immediately.
+        foreach (var key in frequencyCounter1.Keys)
+        {
+            if (!frequencyCounter2.ContainsKey(key * key))
+            {
+                return false;
+            }
+
+            // We also check whether the frequency of each key in the first dictionary matches the frequency of the squared value in the second dictionary.
+            if (frequencyCounter1[key] != frequencyCounter2[key * key])
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
 
--- Tests/00_FrequencyCounterPattern/001.ArrayContainingSquaredValuesTests.cs
+++ Tests/00_FrequencyCounterPattern/001.ArrayContainingSquaredValuesTests.cs
@@ -1,22 +1,24 @@
 using ADS.FrequencyCounterPattern;
 using NUnit.Framework;
 
-namespace Tests.FrequencyCounterPattern
-{
-    [TestFixture]
-    public class ArrayContainingSquaredValuesTests
-    {
-        [Test]
-        [TestCase(new int[] { 2, 3, 4 }, new int[] { 4, 9, 16 })]
-        [TestCase(new int[] { 1, 2, 3 }, new int[] { 1, 4, 9 })]
-        [TestCase(new int[] { 1, 2, 3 }, new int[] { 1, 4 })]
-        [TestCase(new int[] { 1, 2, 3 }, new int[] { 1, 4, 5 })]
-        [TestCase(new int[] { 1, 2, 3 }, new int[] { 1, 4, 9, 10 })]
-        public void ArrayContainingSquaredValues_WhenCalled_ReturnTrueIfSecondArrayHaveAllSquaredValues(int[] firstArray, int[] secondArray)
-        {
-            var result = ArrayContainingSquaredValuesProblem.SecondArrayHaveAllSquaredValues(firstArray, secondArray);
+namespace Tests.FrequencyCounterPattern;
 
-            Assert.That(result, Is.True);
-        }
+[TestFixture]
+public class ArrayContainingSquaredValuesTests
+{
+    [Test]
+    [TestCase(new int[] { 2, 3, 4 }, new int[] { 4, 9, 16 }, true)]
+    [TestCase(new int[] { 1, 2, 3 }, new int[] { 4, 1, 9 }, true)]
+    [TestCase(new int[] { 1, 2, 3 }, new int[] { 1, 4 }, false)]
+    [TestCase(new int[] { 1, 2, 3 }, new int[] { 1, 4, 5 }, false)]
+    [TestCase(new int[] { 1, 2, 3 }, new int[] { 1, 4, 9, 10 }, false)]
+    [TestCase(new int[] { 1, 2, 1 }, new int[] { 4, 4, 1 }, false)]
+    [TestCase(new int[] { 2, 3, 6, 8, 8 }, new int[] { 64, 36, 4, 9, 64 }, true)]
+    public void ArrayContainingSquaredValues_WhenCalled_ReturnExpectedValue(int[] firstArray, int[] secondArray, bool expectedResult)
+    {
+        var result = ArrayContainingSquaredValuesProblem.SameSquared(firstArray, secondArray);
+
+        Assert.That(result, Is.EqualTo(expectedResult));
     }
 }
+
