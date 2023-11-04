e3c241e08b15958b4cb0e8c18c54c75404b0b50b
Ronny Delgado <delgado.ronny@gmail.com>
2023-11-04 14:56:39 -0600

    feat: add solution to pair summing zero
    INtro to Multiple Pointers pattern

---
+++ ADS/01_MultiplePointers/001.PairSumZeroProblem.cs
@@ -0,0 +1,42 @@
+﻿#nullable enable
+
+namespace ADS.MultiplePointers;
+
+public static class PairSumZeroProblem
+{
+    // STATEMENT OF THE PROBLEM
+    // ------------------------
+    // A function which accepts a sorted array of integers.
+    // The function should find the first pair of numbers where the sum is 0.
+    // Return an array that includes both values that sum to zero or null if a pair does not exits.
+
+    public static int[]? SumZero(int[] setOfNumbers)
+    {
+        if (setOfNumbers.Length <= 0) return null;
+
+        int left = 0;
+        int right = setOfNumbers.Length - 1;
+
+        while (left < right)
+        {
+            int sum = setOfNumbers[left] + setOfNumbers[right];
+
+            if (sum == 0)
+            {
+                return new int[] { setOfNumbers[left], setOfNumbers[right] };
+            }
+
+            if (sum > 0)
+            {
+                right--;
+            }
+            else
+            {
+                left++;
+            }
+        }
+
+        return null;
+    }
+}
+
---
+++ Tests/01_MultiplePointers/001.PairSumZeroTests.cs
@@ -0,0 +1,22 @@
+﻿#nullable enable
+
+using ADS.MultiplePointers;
+using NUnit.Framework;
+
+namespace Tests.MultiplePointers;
+
+[TestFixture]
+public static class PairSumZeroTests
+{
+    [Test]
+    [TestCase(new int[] { -3, -2, -1, 0, 1, 2, 3 }, new int[] { -3, 3 })]
+    [TestCase(new int[] { -4, -3, -2, -1, 0, 1, 2, 3, 10 }, new int[] { -3, 3 })]
+    [TestCase(new int[] { -2, 0, 1, 3 }, null)]
+    [TestCase(new int[] { 1, 2, 3 }, null)]
+    public static void SumZero_WhenCalled_ReturnExpectedValue(int[] setOfNumbers, int[]? expectedResult)
+    {
+        var result = PairSumZeroProblem.SumZero(setOfNumbers);
+
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+}
