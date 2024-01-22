14da04b70518c32c626805f78894420f1703d0e5
Ronny Delgado <delgado.ronny@gmail.com>
2024-01-22 09:05:31 -0600

    feat: add duplicates in array challenge with problem and solution

---
+++ ADS/00_FrequencyCounterPattern/004.DuplicatesInArrayProblem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ADS.FrequencyCounterPattern;
+
+public static class DuplicatesInArrayProblem
+{
+    // STATEMENT OF THE PROBLEM
+    // ------------------------
+    // Given list of numbers, find out if there are duplicates.
+
+    public static bool ThereAreDuplicates(int[] listOfNumbers)
+    {
+        // NAIVE SOLUTION
+        // Might use nested loops to compare each item with all the rest of items in the array
+        // Return false if one value is found in the second array
+
+        // BETTER SOLUTION
+        // In a single looop, save in a dictionary each "visited" value.
+        // The key of such dictionary will be the value of the item, and the value of the dictionary will be the index in the array
+        // In each iteration, compare the value with "visited" values in the dictionary and compare
+        // return false if the current value exist in the dictionary
+
+        var visitedItems = new Dictionary<int, int>();
+
+        if (listOfNumbers.Length == 0)
+        {
+            return false;
+        }
+
+        for (var i = 0; i < listOfNumbers.Length; i++)
+        {
+            var currentValue = listOfNumbers[i];
+
+            if (visitedItems.ContainsKey(currentValue))
+            {
+                return true;
+            }
+
+            visitedItems.Add(currentValue, i);
+        }
+
+        return false;
+    }
+}
---
+++ Tests/00_FrequencyCounterPattern/004.DuplicatesInArrayTests.cs
@@ -0,0 +1,21 @@
+﻿using ADS.FrequencyCounterPattern;
+using NUnit.Framework;
+
+namespace Tests.FrequencyCounterPattern;
+
+[TestFixture]
+public class DuplicatesInArrayTests
+{
+    [Test]
+    [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 8 }, true)]
+    [TestCase(new int[] { 1, 2, 3 }, false)]
+    [TestCase(new int[] { 1, 2, 2 }, true)]
+    [TestCase(new int[] { 3, 6, 7, 0, 34, 6, 12, 8, 56, 9, 3 }, true)]
+    public void DuplicatesInArray_WhenCalled_ReturnExpectedValue(int[] listOfNumbers, bool expectedResult)
+    {
+        var result = DuplicatesInArrayProblem.ThereAreDuplicates(listOfNumbers);
+
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+}
+
