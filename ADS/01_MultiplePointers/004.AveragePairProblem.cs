34ebab960900d7bd3d6ba69c6a99ed7c32037521
Ronny Delgado <delgado.ronny@gmail.com>
2024-01-26 13:37:09 -0600

    feat: add problem and solution to the average pair challenge

---
+++ ADS/01_MultiplePointers/004.AveragePairProblem.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+
+namespace ADS.MultiplePointers;
+
+public static class AveragePairProblem
+{
+    // STATEMENT OF THE PROBLEM
+    // ------------------------
+    // Write a function called averagePair. Given a sorted array of integers and a target average, 
+    // determine if there is a pair of values in the array where the average of the pair equals the target average.
+
+    public static bool AveragePair(int[] setOfNumbers, decimal target)
+    {
+        // NAIVE SOLUTION
+        // We would need two nested loops:
+        // The first loop would take one value of the array and compute the average with each item
+        // of the rest of the array (in a second loop).  
+        // When "target" equals the computed average, return true.
+        // At the end of the function, return false (if the pair doesn't exist, it will get until the end of the function)
+
+        // BETTER SOLUTION
+        // Loop one time over the set of numbers
+        // Keep track of each "visited" value in a dictionary.
+        // In each iteration, find the other "required" value to get the target average.
+        // Check if that "required" value is in the dictionary, if so, return true
+        // At the end of the function, return false.
+
+        if (setOfNumbers.Length == 0)
+        {
+            return false;
+        }
+
+        var visitedValues = new Dictionary<decimal, bool>();
+
+        for (var i = 0; i < setOfNumbers.Length; i++)
+        {
+            var requiredValue = (target * 2) - setOfNumbers[i];
+
+            if (visitedValues.ContainsKey(requiredValue))
+            {
+                return true;
+            }
+
+            if (!visitedValues.ContainsKey(setOfNumbers[i]))
+            {
+                visitedValues.Add(setOfNumbers[i], true);
+            }
+        }
+
+        return false;
+    }
+}
+
---
+++ Tests/01_MultiplePointers/004.AveragePairTests.cs
@@ -0,0 +1,22 @@
+﻿#nullable enable
+
+using ADS.MultiplePointers;
+using NUnit.Framework;
+
+namespace Tests.MultiplePointers;
+
+[TestFixture]
+public static class AveragePairTests
+{
+    [Test]
+    [TestCase(new int[] { 1, 2, 3 }, 2.5, true)]
+    [TestCase(new int[] { 1, 3, 3, 5, 6, 7, 10, 12, 19 }, 8, true)]
+    [TestCase(new int[] { -1, 0, 3, 4, 5, 6 }, 4.1, false)]
+    [TestCase(new int[] { }, 4, false)]
+    public static void AveragePair_WhenCalled_ReturnExpectedValue(int[] setOfNumbers, decimal target, bool expectedResult)
+    {
+        var result = AveragePairProblem.AveragePair(setOfNumbers, target);
+
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+}
