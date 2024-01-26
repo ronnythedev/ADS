05a4f3c18c569bc3d87c48867da50c97998231ba
Ronny Delgado <delgado.ronny@gmail.com>
2024-01-26 15:58:41 -0600

    feat: add problem and solution for the subsequence challenge

---
+++ ADS/01_MultiplePointers/005.SubsequenceProblem.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace ADS.MultiplePointers;
+
+public static class SubsequenceProblem
+{
+    // STATEMENT OF THE PROBLEM
+    // ------------------------
+    // Write a function called IsSubsequence which takes in two strings and checks whether the 
+    // characters in the first string forma subsequence of the characters in the second string,
+    // not matter if there are characters in between in the second string.
+
+    public static bool IsSubsequence(string firstString, string secondString)
+    {
+        // NAIVE SOLUTION
+
+
+        // BETTER SOLUTION
+        // Loop over the second string
+        // Keep a counter that increases only when the character in the second string is found in the first string in [counter] position
+        // Compare the lenght of the first string with the counter
+
+        if (firstString.Length == 0 || secondString.Length == 0)
+        {
+            return false;
+        }
+
+        var counter = 0;
+        foreach (char character in secondString)
+        {
+            var character2 = firstString[counter];
+            if (character == character2)
+            {
+                counter += 1;
+            }
+
+            if (counter == firstString.Length)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
---
+++ Tests/01_MultiplePointers/005.SubsequenceTests.cs
@@ -0,0 +1,22 @@
+﻿#nullable enable
+
+using ADS.MultiplePointers;
+using NUnit.Framework;
+
+namespace Tests.MultiplePointers;
+
+[TestFixture]
+public static class SubsequenceTests
+{
+    [Test]
+    [TestCase("abc", "adbxcq", true)]
+    [TestCase("hello", "hello world", true)]
+    [TestCase("sing", "sting", true)]
+    [TestCase("abc", "acb", false)]
+    public static void IsSubsequence_WhenCalled_ReturnExpectedValue(string firstString, string secondString, bool expectedResult)
+    {
+        var result = SubsequenceProblem.IsSubsequence(firstString, secondString);
+
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+}
