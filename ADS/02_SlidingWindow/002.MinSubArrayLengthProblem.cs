fe152226a8004788edbbf748770b94a86c35bdb7
Ronny Delgado <delgado.ronny@gmail.com>
2024-01-26 18:21:47 -0600

    feat: add problem and test for min sub array length challenge

---
+++ ADS/02_SlidingWindow/002.MinSubArrayLengthProblem.cs
@@ -0,0 +1,63 @@
+﻿namespace ADS.SlidingWindow;
+
+public static class MinSubArrayLengthProblem
+{
+    // STATEMENT OF THE PROBLEM
+    // ------------------------
+    // Write a funtion called GetMinSubarrayLength which accepts an array of positive integers
+    // and a positive integer "k"
+    // Return the minimal length of a contiguos subarray of which the sum is greater than or equal to "k"
+
+    public static int GetMinSubarrayLength(int[] listOfNumbers, int k)
+    {
+        // NAIVE SOLUTION
+
+        // BETTER SOLUTION
+        // 1. Always use a while loop with the "lowerPointer" and the length - 1 of the array
+        // 2. Always increase "upperPointer" at the end of the loop
+        // 3. The body of the while loop will always compute something (in this case a sum) 
+        // and must have a "reset" validation (in this case if sum >= k)
+        // 4. In the reset area, usually "upperPointer" will point to current "lowerPointer"
+        // and "lowerPointer" will increase in one.
+        // 5. In this pattern, "lowerPointer" will increase slowly while "upperPointer" will increase faster
+        // but "upperPointer" will be "reset" several times during the execution
+
+        if (listOfNumbers.Length == 0)
+        {
+            return 0;
+        }
+
+        var minLength = listOfNumbers.Length;
+        var lowerPointer = 0;
+        var upperPointer = 0;
+        var lengthCounter = 0;
+        int sum = 0;
+
+        while (lowerPointer < listOfNumbers.Length)
+        {
+            lengthCounter++;
+            sum += listOfNumbers[upperPointer];
+
+            if (sum >= k)
+            {
+                if (lengthCounter < minLength)
+                {
+                    minLength = lengthCounter;
+                }
+
+                upperPointer = lowerPointer;
+                lowerPointer++;
+                lengthCounter = 0;
+                sum = 0;
+            }
+
+            if (upperPointer < listOfNumbers.Length - 1)
+            {
+                upperPointer++;
+            }
+        }
+
+        return (minLength == listOfNumbers.Length) ? 0 : minLength;
+
+    }
+}
---
+++ Tests/02_SlidingWindow/002.MinSubArrayLengthTests.cs
@@ -0,0 +1,26 @@
+﻿namespace Tests.SlidingWindow;
+
+using ADS.SlidingWindow;
+using NUnit.Framework;
+
+public static class MinSubArrayLengthTests
+{
+    [TestFixture]
+    public class GetMinSubarrayLengthTests
+    {
+        [Test]
+        [TestCase(new int[] { 2, 3, 1, 2, 4, 3 }, 7, 2)]
+        [TestCase(new int[] { 2, 1, 6, 5, 4 }, 9, 2)]
+        [TestCase(new int[] { 3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19 }, 52, 1)]
+        [TestCase(new int[] { 1, 4, 16, 22, 5, 7, 8, 9, 10 }, 39, 3)]
+        [TestCase(new int[] { 1, 4, 16, 22, 5, 7, 8, 9, 10 }, 55, 5)]
+        [TestCase(new int[] { 4, 3, 3, 8, 1, 2, 3 }, 11, 2)]
+        [TestCase(new int[] { 1, 4, 16, 22, 5, 7, 8, 9, 10 }, 95, 0)]
+        public void GetMinSubarrayLength_WhenCalled_ReturnExpectedValue(int[] listOfNumbers, int k, int expectedResult)
+        {
+            var result = MinSubArrayLengthProblem.GetMinSubarrayLength(listOfNumbers, k);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
